f6191a148fcceeb97da0d625f0512890ab642017
Bùi Đức Chương <buichuong.bdc@gmail.com>
2026-07-29 16:08:14 +0700

    chore: sync Flutter generated files and TV5 docs

--- 05_Development/CareBridgeMobileApp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ 05_Development/CareBridgeMobileApp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import firebase_messaging
 import flutter_secure_storage_macos
 import geolocator_apple
 import google_sign_in_ios
+import shared_preferences_foundation
 import url_launcher_macos
 import zego_express_engine
 
@@ -27,6 +28,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   ZegoExpressEnginePlugin.register(with: registry.registrar(forPlugin: "ZegoExpressEnginePlugin"))
 }
